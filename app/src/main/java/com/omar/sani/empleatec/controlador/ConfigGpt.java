8dcf52f7d3cc0198396d4a8e54d31201fef5ed1c
sonick-s <omigc4@gmail.com>
2024-07-12 09:58:54 -0500

    Añado configuracion de ChatGPT API

---
+++ app/src/main/java/com/omar/sani/empleatec/controlador/ConfigGpt.java
@@ -0,0 +1,7 @@
+package com.omar.sani.empleatec.controlador;
+
+public class ConfigGpt {
+
+        public static final String CHATAPI_KEY = "tu_clave_api_aqui"; // Reemplaza esto con tu clave API real
+
+}
